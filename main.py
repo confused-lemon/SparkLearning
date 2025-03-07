32dc4c0cdc62b59f17e5b207ab48072d98b7aed6
Joe <jvwalsh@protonmail.com>
2025-03-06 20:21:25 -0500

    Added new method instances and write to csv

--- main.py
+++ main.py
@@ -32,15 +32,21 @@ def main(is_remote: bool):
     "driver" : "org.postgresql.Driver"
     }
 
-    # driver = HighestScoringPosts(spark_session, connection, credentials_dict)
+    driver = HighestScoringPosts(spark_session, connection, credentials_dict)
+    all_subs = driver.get_subs()
+    all_subs.write.csv('output/subs', header=True, mode='overwrite')
+
+    election_analysis = BulkTitleAnalysis(spark_session, connection, credentials_dict)
+    election_post_data = election_analysis.sent_analysis_of_political_posts_six_weeks()
+    election_post_data.write.csv('output/six_weeks_election', header=True, mode='overwrite')
     # month_result = driver.get_highest_scores_last_month()
     
     # result =  driver.get_highest_scores_last_week()
     # month_results = driver.get_highest_scores_last_month()
     # result.show()
     # month_results.coalesce(1).write.csv('output/', header=True, mode='overwrite')
-    title_analysis = BulkTitleAnalysis(spark_session, connection, credentials_dict)
-    title_analysis.break_into_year_months()
+    # title_analysis = BulkTitleAnalysis(spark_session, connection, credentials_dict)
+    # title_analysis.break_into_year_months()
     spark_session.stop()
 
 if __name__ == '__main__':

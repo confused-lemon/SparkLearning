b944c33ddd86d56fe2b5fbed0de5156a406a0a8e
walshj9 <jw051098@gmail.com>
2025-03-15 15:48:42 -0400

    Generates data frame with pos/neg/neu scores for a title

--- compute/handling.py
+++ compute/handling.py
@@ -1,15 +1,25 @@
-import sys, os
-import numpy as np
+import platform, os
+from pathlib import Path
 import pandas as pd
 from glob import glob
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-csv_files = glob('output/six_weeks_election/*.csv')
-title_analizer = SentimentIntensityAnalyzer()
-
-def title_sentiment_analysis_scores(self, title: str):
-        """Returns sentiment analysis of a given post title"""
-        return title_analizer.polarity_scores(title.replace('#', ' ')) # polarity_scores ignores hashtagged words
+if platform.system() == "Windows": # windows was being annoying
+    script_location = Path(__file__).resolve().parent
+    csv_path = script_location.parent / "output" / "six_weeks_election" / "*.csv"
+    csv_files = glob(str(csv_path))
+else:
+    csv_files = glob('output/six_weeks_election/*.csv')
+title_analyzer = SentimentIntensityAnalyzer()
+def title_sentiment_analysis_scores(row):
+        """Adds sentiment analysis of a given post title to the unique Id frame"""
+        scores = title_analyzer.polarity_scores(row['title'].replace('#', ' ')) # polarity_scores ignores hashtagged words
+        return pd.Series(
+            {'id': row['id'],
+            'title': row['title'],
+            'pos': scores['pos'],
+            'neg': scores['neg'],
+            'neu': scores['neu']})
 
 dfs = []
 for file in csv_files:
@@ -17,5 +27,6 @@ for file in csv_files:
     dfs.append(df)
 master_frame = pd.concat(dfs, ignore_index=True)
 
+unique_titles = master_frame[['id', 'title', 'snapshot_time_utc']].drop_duplicates()
 
-master_frame.sort_values("score").groupby("id")
+scored_titles = unique_titles.apply(lambda row: title_sentiment_analysis_scores(row), axis=1)

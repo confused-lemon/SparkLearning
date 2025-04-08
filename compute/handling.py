30fcaf1f15945fecdfc10444fa279dc27a7fdda1
Joe <jvwalsh@protonmail.com>
2025-04-08 09:24:12 -0400

    Updated paths to outisde dir drop dupes by id col

--- compute/handling.py
+++ compute/handling.py
@@ -1,16 +1,21 @@
 import platform, os
 from pathlib import Path
-import pandas as pd
 from glob import glob
+import pandas as pd
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 if platform.system() == "Windows": # windows was being annoying
-    script_location = Path(__file__).resolve().parent
-    csv_path = script_location.parent / "output" / "six_weeks_election" / "*.csv"
+    home_dir = Path.home()
+    csv_path = home_dir.parent / "data" / "output" / "six_weeks_election" / "*.csv"
     csv_files = glob(str(csv_path))
 else:
-    csv_files = glob('output/six_weeks_election/*.csv')
+    path = os.path.expanduser("~/data/output/six_weeks_election/*.pdf")
+    csv_files = glob(path)
+    if len(csv_files) == 0:
+        raise ValueError(f"No .csv files found in dir: {path}")
+
 title_analyzer = SentimentIntensityAnalyzer()
+
 def title_sentiment_analysis_scores(row):
         """Adds sentiment analysis of a given post title to the unique Id frame"""
         scores = title_analyzer.polarity_scores(row['title'].replace('#', ' ')) # polarity_scores ignores hashtagged words
@@ -27,6 +32,5 @@ for file in csv_files:
     dfs.append(df)
 master_frame = pd.concat(dfs, ignore_index=True)
 
-unique_titles = master_frame[['id', 'title', 'snapshot_time_utc']].drop_duplicates()
-
+unique_titles = master_frame[['id', 'title', 'snapshot_time_utc']].drop_duplicates(subset=['id'])
 scored_titles = unique_titles.apply(lambda row: title_sentiment_analysis_scores(row), axis=1)
